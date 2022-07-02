c3443ed92f43d73baf5322cc018b8782adfeebd3
Aadya21370 <aadya21370@iiitd.ac.in>
2022-07-02 10:10:51 +0530

    changes made to asm.py

--- asm.py
+++ asm.py
@@ -1,68 +1,69 @@
-def parse_vars(lst, vars_dict, PC):
+PC = 00000000
+type_a = {"add": "10000", "sub": "10001", "mul": "10110", "xor": "11010", "or": "11011", "and": "11100", }
+type_c = {"div": "10111"}
+type_e = {"jmp": "11111", "jlt": "01100"}
+reg = {"r1": "000", "r2": "001", "r3": "010", "r4":"011"}
+vars = {}
+labels = {} # with line number where labels come
+count = 0
+
+
+def parse_vars(lst, vars_dict, PC, count):
     for i in lst:
         if "var" not in i.lower():
             PC+=1
         else:
             vars_dict[f'var {count}'] = i[4]
-
-
-PC = 00000000
-type_a = {"add": "10000", "mul": "10110"}
-type_c = {"div": "10111"}
-type_e = {"jmp": "11111", "jlt": "01100"}
-reg = {"r1": "000", "r2": "001", "r3": "010", "r4":"011"}
-vars = {}
-count = 0
+            count+=1
 
 
 with open('trial.txt') as f:
-    inp = f.readlines()
-    c = inp.count("\n")
+    new = f.readlines()
+    c = new.count("\n")
     for i in range(c):
-        inp.remove("\n")
-    for i in range(len(inp)):
-        inp[i] = " ".join(inp[i].split())
+        new.remove("\n")
+    for i in range(len(new)):
+        new[i] = " ".join(new[i].split())
 
-    # parse_vars(inp, vars, PC)
+    parse_vars(new, vars, PC, count)
 
-    new = []
-    for i in inp:
+    inp = []
+    for i in new:
         l = i.split()
-        new.append(l)
-    print(new)
+        inp.append(l)
+    print(inp)
 
-    if type_a not in i and type_c not in i and type_e not in i and "var" not in i:
-        print(f'Error in line: Undefined Instruction Name')
-    elif inp[0].lower() in type_a:
-        if len(inp) == 4:
-            if inp[1].lower() in reg and inp[2].lower() in reg and inp[3].lower() in reg:
-                op = type_a[inp[0]]
-                r1 = reg[inp[1].lower()]
-                r2 = reg[inp[2].lower()]
-                r3 = reg[inp[3].lower()]
+for i in range(len(inp)):
+    if inp[i][0].lower() in type_a:
+        if len(inp[i]) == 4:
+            if inp[i][1].lower() in reg and inp[i][2].lower() in reg and inp[i][3].lower() in reg:
+                op = type_a[inp[i][0]]
+                r1 = reg[inp[i][1].lower()]
+                r2 = reg[inp[i][2].lower()]
+                r3 = reg[inp[i][3].lower()]
                 print(op+"00"+r1+r2+r3)
             else:
                 print(f'Error in line: Undefined Register Name')
         else:
             print(f'Error in line: Wrong format of instruction')
-    elif inp[0].lower() in type_c:
-        if len(inp) == 3:
-            if inp[1].lower() in reg and inp[2].lower() in reg:
+    elif inp[i][0].lower() in type_c:
+        if len(inp[i]) == 3:
+            if inp[i][1].lower() in reg and inp[i][2].lower() in reg:
                 op = type_c[inp[0]]
-                r1 = reg[inp[1].lower()]
-                r2 = reg[inp[2].lower()]
+                r1 = reg[inp[i][1].lower()]
+                r2 = reg[inp[i][2].lower()]
                 print(op+"00000"+r1+r2)
             else:
                 print(f'Error in line: Undefined Register Name')
         else:
             print(f'Error in line: Wrong format of instruction')
-    elif inp[0].lower() in type_e:
-        if len(inp) == 2:
-            if len(inp[1]) == 8:
-                op = type_e[inp[0]]
-                print(op+"000"+inp[1])
+    elif inp[i][0].lower() in type_e:
+        if len(inp[i]) == 2:
+            if len(inp[i][1]) == 8:
+                op = type_e[inp[i][0]]
+                print(op+"000"+inp[i][1])
             ## condition to check if mem_addr is accessible n acceptable (?)
         else:
             print(f'Error in line: Wrong format of instruction')
-    elif inp[0].lower() == "hlt":
+    elif inp[i][0].lower() == "hlt":
         print("0101000000000000")
