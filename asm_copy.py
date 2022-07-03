1720fc66e8c4689209b95c32172385042e4050a6
vanya1415 <99266006+vanya1415@users.noreply.github.com>
2022-07-03 15:30:20 +0530

    Create asm_copy.py

--- asm_copy.py
+++ asm_copy.py
@@ -1,16 +1,17 @@
-import sys
 PC = 0
 type_a = {"add": "10000", "sub": "10001", "mul": "10110", "xor": "11010", "or": "11011", "and": "11100"}
 type_b = {"ls":"11001", "rs":"11000", "mov":"10010"} #mov r1 imm
 type_c = {"div": "10111", "not": "11101", "cmp": "11110", "mov": "10011"} # mov r1 r2
-type_d = {"ld": "10100", "st": "10101"}
+type_d = {"st": "10100", "ld": "10101"}
 type_e = {"jmp": "11111", "jlt": "01100", "je": "01111", "jgt": "01101"}
 reg = {"r0": "000", "r1": "001", "r2": "010", "r3": "011", "r4": "100", "r5": "101", "r6": "110", "r7": "111"}
 vars = {}
 labels = {} # with line number where labels come
 machine_code = []
 vars_count = 0 
-flag = True
+flag = False
+
+opcode={"add": "10000", "sub": "10001", "mul": "10110", "xor": "11010", "or": "11011", "and": "11100", "ls":"11001", "rs":"11000", "mov":"10010","div": "10111", "not": "11101", "cmp": "11110", "mov": "10011","st": "10100", "ld": "10101","jmp": "11111", "jlt": "01100", "je": "01111", "jgt": "01101"}
 
 def check_bin(check_str):   
     flag = True
@@ -42,7 +43,7 @@ def DecBin(string):
         Flag=False
   
     #converting decimal to binary
-    if Flag and Flag1:      # typo in Flag1 (?)     # also can use bin() function instead
+    if Flag:   
         num=int(string)
         Bnumlst=[]
         q=1
@@ -63,38 +64,65 @@ def DecBin(string):
         return("Enter the correct number")
 
     # parse_vars(lst1, vars, PC)
+def BinDec(string):
+    #Checking if input number is binary.
+    if string.isdigit():
+        lst=[int(i) for i in string]
+        for i in lst:
+            if i>=0 and i<=1:
+                Flag=True
+            else:
+                Flag=False
+                break
+    else:
+        Flag=False
+        
 
-# f=input()
-# lines=f.split("\n")
-# inst_lst=[]
-# for i in lines:
-#     new_line=i.split()
-#     inst_lst.append(new_line)
+
+    #Converting binary number to decimal number.
+    if Flag:
+        lst=[int(i) for i in string]
+        DecNum=0
+        highestPower=len(lst)
+        for i in range(len(lst)):
+            DecNum+=(lst[i]*(2**(highestPower-1)))
+            highestPower-=1
+        return(DecNum)
+
+    if not(Flag):
+        return("Enter the correct number")
+
+    
+f=input()
+lines=f.split("\n")
+inst_lst=[]
+for i in lines:
+    new_line=i.split()
+    inst_lst.append(new_line)
 
 # alternative code to read multi line input
-s = sys.stdin.read()
-line = s.split("\n")
-c = line.count("")
-for i in range(c):
-    line.remove("")
-parse_vars(line, vars, PC, vars_count)
-inst_lst = []
-for i in line:
-    ele = i.split()
-    inst_lst.append(ele)
+# import sys
+# s = sys.stdin.read()
+# line = s.split("\n")
+# inst_lst = []
+# for i in line:
+#     ele = i.split()
+#     inst_lst.append(ele)
+lab_dic={}
+for i in range(len(inst_lst)):
+    elif inst_lst[i][0][-1]==":" and inst_lst[i][1] in opcode:
+        lab_dic[DecBin(string(i))]=inst_lst[i][0][:-1].lower()
 
+if len(inst_lst[-1]) != 1 or inst_lst[-1][0] != "hlt":
+        flag = True
+        print(f'Error in line {len(inst_lst)}: Invalid/Absent hlt declaration')
 
-if len(inst_lst2[-1]) != 1 or inst_lst2[-1][0] != "hlt":
-        flag = False
-        print(f'Error in line {len(inst_lst2)}: Invalid/Absent hlt declaration')
-
-if flag == True:
+if flag == False:
     for i in range(len(inst_lst)):
-        if len(inst_lst[i]) == 0:
-            continue
         if inst_lst[i][0].lower() in type_a:
             flag_a = True
             if len(inst_lst[i]) != 4:
+                flag_a=False
                 print(f'Error in line {i+1}: Wrong Instruction syntax for {inst_lst[i][0].lower()}')
             if inst_lst[i][1].lower() not in reg or inst_lst[i][2].lower() not in reg or inst_lst[i][3].lower() not in reg:
                 flag_a = False
@@ -106,52 +134,50 @@ if flag == True:
                 r3 = reg[inst_lst[i][3].lower()]
                 machine_code.append(op+"00"+r1+r2+r3)
             
-        elif inst_lst[i][0] in type_b and inst_lst[i][2][1]=="$":
+        elif inst_lst[i][0].lower() in type_b and inst_lst[i][2][1]=="$":
             flag_b=True
             if len(inst_lst[i])!=3:
                 flag_b=False
-                print(f'Error in line {i}: Number of operands exceed requirement')
-            if int(inst_lst[i][1][3])<0 or int(inst_lst[i][1][3])>6:
+                print(f'Error in line {i+1}: Number of operands exceed requirement')
+            if int(inst_lst[i][1][1])<0 or int(inst_lst[i][1][1])>6:
                 flag_b=False
-                print(f'Error in line {i}: Undefined Register name')
-            if inst_lst[i][2]=="FLAG":
+                print(f'Error in line {i+1}: Undefined Register name')
+            if inst_lst[i][2].lower()=="flag":
                 flag_b=False
-                print(f'Error in line {i}: Illegal use of flags register')
-            Imm=""
-            for k in range(1,len(inst_lst[i][2])):
-                Imm+=inst_lst[i][2][k]
+                print(f'Error in line {i+1}: Illegal use of flags register')
+            Imm=inst_lst[i][2][1:]
             if int(Imm)<0 or int(Imm)>255:
                 flag_b=False
-                print(f'Error in line {i}: Illegal immediate values')
+                print(f'Error in line {i+1}: Illegal immediate values')
             if flag_b:
-                op=type_b[inst_lst[i][0]]
+                op=type_b[inst_lst[i][0].lower()]
                 r1=reg[inst_lst[i][1]]
                 Im=DecBin(Imm)
                 machine_code.append(op+r1+Im)
                
         elif inst_lst[i][0] in type_c:
             flag_c=True
-            if len(inst_lst[i]) == 3:
+            if len(inst_lst[i]) != 3:
                 flag_c=False
-                print(f'Error in line {i}: Number of operand exceed requirement')
-            if int(inst_lst[i][1][3])<0 or int(inst_lst[i][1][3])>6:
+                print(f'Error in line {i+1}: Number of operand exceed requirement')
+            if int(inst_lst[i][1][1])<0 or int(inst_lst[i][1][1])>6:
                 flag_c=False
-                print(f'Error in line {i}: Undefined Register name')
-            if inst_lst[i][2]=="FLAG":
+                print(f'Error in line {i+1}: Undefined Register name')
+            if inst_lst[i][2].lower()=="flag":
                 if inst_lst[i][0]!="mov":
                     flag_c=False
-                    print(f'Error in line {i}: Illegal use of flag register')
-            if inst_lst[i][1]=="FLAG":
+                    print(f'Error in line {i+1}: Illegal use of flag register')
+            if inst_lst[i][1].lower()=="flag":
                 flag_c=False
-                print(f'Error in line {i}: Illegal use of flag register')
-            if inst_lst[i][2]!="FLAG":
-                if int(inst_lst[i][1][3])<0 or int(inst_lst[i][1][3])>6:
+                print(f'Error in line {i+1}: Illegal use of flag register')
+            if inst_lst[i][2].lower()!="flag":
+                if int(inst_lst[i][1][1])<0 or int(inst_lst[i][1][1])>6:
                     flag_c=False
-                    print(f'Error in line {i}: Undefined register name')
+                    print(f'Error in line {i+1}: Undefined register name')
             if flag_c:
                 op=type_c[inst_lst[i][0]]
                 r1=reg[inst_lst[i][1]]
-                if inst_lst[i][2]=="FLAG":
+                if inst_lst[i][2].lower()=="flag":
                     r2="111"
                 else:
                     r2=reg[inst_lst[i][2]]
@@ -161,12 +187,10 @@ if flag == True:
             if len(inst_lst[i]) == 3:
                 if inst_lst[i][1].lower() in reg:
                     if inst_lst[i][2] in vars.values():
-                        op = type_d[inst_lst[i][0]]
+                        op = type_a[inst_lst[i][0]]
                         r1 = reg[inst_lst[i][1].lower()]
-                        PC += 1
-                        b = bin(PC)
-                        mem = str(b).replace("0b", "")
-                        mem = format(int(mem), '#08')
+                        PC = PC + len(inst_lst) - 1
+                        mem = PC
                         machine_code.append(op+r1+mem)
                     elif inst_lst[i][2] in labels:
                         print(f'Error in line {i+1}: Use of labels as variables')
@@ -177,29 +201,30 @@ if flag == True:
             else:
                 print(f'Error in line {i+1}: Wrong Instruction syntax for {inst_lst[i][0].lower()}')
                 
-        elif inst_lst[i][1].lower() in type_e:
+        elif inst_lst[i][0].lower() in type_e:
             flag_e=True
-            new_st=""
-            if len(inst_lst[i]) == 2:
-                new_st.append(type_e[inst_lst[i][0]])
-            else:
+            if len(inst_lst[i])!=2:
                 flag_e=False
-                machine_code=(f'Error in line: Wrong format of instruction')
-                break
-            if len(inst_lst[i][1]) == 8:
-                new_st.append(inst_lst[i][1])
-                # condition to check if mem_addr is accessible (?)
-            else:
+                print(f' Error in line {i+1}: Number of operands exceed requirement')
+            if inst_lst[i][1] not in lab_dic:
                 flag_e=False
-                machine_code=(f'Error in line: Memory address is not accessible/acceptable')
-                break   
-                                    
+                print(f'Error in line {i+1}: memory address is not a label')
+
             if flag_e:
-                machine_code.append(new_st) 
-                
-        elif inst_lst[i][0].lower() == "hlt":
-            machine_code.append("0101000000000000")    
+                op=type_e[inst_lst[i][0]]
+                mem=inst_lst[i][1]
+                machine_code.append(op+mem)
             
-if flag:
+               
+            
+                
+        elif inst_lst[i][0].lower() == "var":
+            vars[inst_lst[i][1]]=0
+
+        elif inst_lst[i][0].lower() == "hlt":
+            machine_code.append("0101000000000000")           
+        elif inst_lst[i][0] == "":
+            continue
+if not flag:
     for i in machine_code:
         print(i, end = "\n")
