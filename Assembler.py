bfd4e9ccd5f0fd7a359fa25a2b85c8b6599f49fe
vanya1415 <99266006+vanya1415@users.noreply.github.com>
2022-07-06 19:31:12 +0530

    Update Assembler.py

--- Assembler.py
+++ Assembler.py
@@ -20,6 +20,7 @@ flag_c=True
 flag_d = True
 flag_e = True
 flag_m=True
+flag_h=False
 
 def check_bin(check_str):   
     flag = True
@@ -108,144 +109,146 @@ for i in range(len(inst_lst2)):
 
 if flag == True:
     for i in range(len(inst_lst2)):
+        if not(flag_h):
         
-        if inst_lst2[i][0][-1]==":":
-            label=inst_lst2[i][0]
-            inst_lst2[i].pop(0)
-            flag_l=True
-        if inst_lst2[i][0].lower() in type_a:
-            flag_a = True
-            if len(inst_lst2[i]) == 4:
-                if inst_lst2[i][1].lower() in reg and inst_lst2[i][2].lower() in reg and inst_lst2[i][3].lower() in reg:
-                    op = type_a[inst_lst2[i][0]]
-                    r1 = reg[inst_lst2[i][1].lower()]
-                    r2 = reg[inst_lst2[i][2].lower()]
-                    r3 = reg[inst_lst2[i][3].lower()]
-                    machine_code.append(op+"00"+r1+r2+r3)
+            if inst_lst2[i][0][-1]==":":
+                label=inst_lst2[i][0]
+                inst_lst2[i].pop(0)
+                flag_l=True
+            if inst_lst2[i][0].lower() in type_a:
+                flag_a = True
+                if len(inst_lst2[i]) == 4:
+                    if inst_lst2[i][1].lower() in reg and inst_lst2[i][2].lower() in reg and inst_lst2[i][3].lower() in reg:
+                        op = type_a[inst_lst2[i][0]]
+                        r1 = reg[inst_lst2[i][1].lower()]
+                        r2 = reg[inst_lst2[i][2].lower()]
+                        r3 = reg[inst_lst2[i][3].lower()]
+                        machine_code.append(op+"00"+r1+r2+r3)
+                    else:
+                        print(f'Error in line {i+1}: Undefined Register name')
+                        flag_a = False
+                        break
                 else:
-                    print(f'Error in line {i+1}: Undefined Register name')
+                    print(f'Error in line {i+1}: Wrong Instruction syntax for {inst_lst[i][0].lower()}')
                     flag_a = False
                     break
-            else:
-                print(f'Error in line {i+1}: Wrong Instruction syntax for {inst_lst[i][0].lower()}')
-                flag_a = False
-                break
                     
                     
-        elif inst_lst2[i][0].lower() in type_b and inst_lst2[i][2][0]=="$":
-            flag_b = True
-            if len(inst_lst2[i])!=3:
-                flag_b=False
-                print(f'Error in line {i+1}: Number of operands exceed requirement')
-            if int(inst_lst2[i][1][1])<0 or int(inst_lst2[i][1][1])>6:
-                flag_b=False
-                print(f'Error in line {i+1}: Undefined Register name')
-            if inst_lst2[i][2].lower()=="flag":
-                flag_b=False
-                print(f'Error in line {i+1}: Illegal use of flags register')
-            Imm=inst_lst2[i][2][1:]
-            if int(Imm)<0 or int(Imm)>255:
-                flag_b=False
-                print(f'Error in line {i+1}: Illegal immediate values')
-            if flag_b:
-                op=type_b[inst_lst2[i][0].lower()]
-                r1=reg[(inst_lst2[i][1]).lower()]
-                Im=DecBin(Imm)
-                machine_code.append(op+r1+Im)
+            elif inst_lst2[i][0].lower() in type_b and inst_lst2[i][2][0]=="$":
+                flag_b = True
+                if len(inst_lst2[i])!=3:
+                    flag_b=False
+                    print(f'Error in line {i+1}: Number of operands exceed requirement')
+                if int(inst_lst2[i][1][1])<0 or int(inst_lst2[i][1][1])>6:
+                    flag_b=False
+                    print(f'Error in line {i+1}: Undefined Register name')
+                if inst_lst2[i][2].lower()=="flag":
+                    flag_b=False
+                    print(f'Error in line {i+1}: Illegal use of flags register')
+                Imm=inst_lst2[i][2][1:]
+                if int(Imm)<0 or int(Imm)>255:
+                    flag_b=False
+                    print(f'Error in line {i+1}: Illegal immediate values')
+                if flag_b:
+                    op=type_b[inst_lst2[i][0].lower()]
+                    r1=reg[(inst_lst2[i][1]).lower()]
+                    Im=DecBin(Imm)
+                    machine_code.append(op+r1+Im)
             
-        elif inst_lst[i][0] in type_c:
-            flag_c = True
-            if len(inst_lst2[i]) != 3:
-                flag_c=False
-                print(f'Error in line {i+1}: Number of operand exceed requirement')
-            if not(inst_lst2[i][1][1].isnumeric()) or int(inst_lst2[i][1][1])<0 or int(inst_lst2[i][1][1])>6:
-                flag_c=False
-                print(f'Error in line {i+1}: Undefined Register name')
-            if inst_lst2[i][2].lower()=="flag":
-                if inst_lst2[i][0]!="mov":
+            elif inst_lst[i][0] in type_c:
+                flag_c = True
+                if len(inst_lst2[i]) != 3:
+                    flag_c=False
+                    print(f'Error in line {i+1}: Number of operand exceed requirement')
+                if not(inst_lst2[i][1][1].isnumeric()) or int(inst_lst2[i][1][1])<0 or int(inst_lst2[i][1][1])>6:
+                    flag_c=False
+                    print(f'Error in line {i+1}: Undefined Register name')
+                if inst_lst2[i][2].lower()=="flag":
+                    if inst_lst2[i][0]!="mov":
+                        flag_c=False
+                        print(f'Error in line {i+1}: Illegal use of flag register')
+                if inst_lst2[i][1].lower()=="flag":
                     flag_c=False
                     print(f'Error in line {i+1}: Illegal use of flag register')
-            if inst_lst2[i][1].lower()=="flag":
-                flag_c=False
-                print(f'Error in line {i+1}: Illegal use of flag register')
-            if flag_c:
-                op=type_c[inst_lst2[i][0]]
-                r1=reg[(inst_lst2[i][1]).lower()]
-                if inst_lst2[i][2].lower()=="flag":
-                    r2="111"
-                else:
-                    r2=reg[(inst_lst2[i][2]).lower()]
-                machine_code.append(op+"00000"+r1+r2)  
+                if flag_c:
+                    op=type_c[inst_lst2[i][0]]
+                    r1=reg[(inst_lst2[i][1]).lower()]
+                    if inst_lst2[i][2].lower()=="flag":
+                        r2="111"
+                    else:
+                        r2=reg[(inst_lst2[i][2]).lower()]
+                    machine_code.append(op+"00000"+r1+r2)  
                 
-        elif inst_lst2[i][0].lower() in type_d:  
-            flag_d = True
-            if len(inst_lst2[i]) == 3:
-                if inst_lst2[i][1].lower() in reg:
-                    if inst_lst2[i][2] in vars:
-                        op = type_d[inst_lst2[i][0]]
-                        r1 = reg[inst_lst2[i][1].lower()]
-                        mem = vars[inst_lst2[i][2].lower()]
-                        machine_code.append(op + r1 + mem)
-                    elif inst_lst2[i][2] in lab_dic:
-                        print(f'Error in line {i+1}: Use of labels as variables')
+            elif inst_lst2[i][0].lower() in type_d:  
+                flag_d = True
+                if len(inst_lst2[i]) == 3:
+                    if inst_lst2[i][1].lower() in reg:
+                        if inst_lst2[i][2] in vars:
+                            op = type_d[inst_lst2[i][0]]
+                            r1 = reg[inst_lst2[i][1].lower()]
+                            mem = vars[inst_lst2[i][2].lower()]
+                            machine_code.append(op + r1 + mem)
+                        elif inst_lst2[i][2] in lab_dic:
+                            print(f'Error in line {i+1}: Use of labels as variables')
+                            flag_d = False
+                            break
+                        else:
+                            print(f'Error in line {i+1}: Use of undefined variable')
+                            flag_d = False
+                            break
+                    else:
+                        print(f'Error in line {i+1}: Undefined Register name')
+                        flag_d = False
+                        break
+                else:
+                    print(f'Error in line {i+1}: Wrong Instruction syntax for {inst_lst[i][0].lower()}')
+                    flag_d = False
+                    break
+        
+            elif inst_lst2[i][0].lower() in type_e:
+                flag_e = True
+                if len(inst_lst2[i]) == 2:
+                    if inst_lst2[i][1].lower() in lab_dic:
+                        op = type_e[inst_lst2[i][0]]
+                        mem = lab_dic[inst_lst2[i][1].lower()]
+                        machine_code.append(op+"000"+mem)
+                    elif inst_lst2[i][1].lower() in vars:
+                        print(f'Error in line {i+1}: Use of variables as labels')
                         flag_d = False
                         break
                     else:
-                        print(f'Error in line {i+1}: Use of undefined variable')
-                        flag_d = False
+                        print(f'Error in line {i+1}: Memory address is not a label')
+                        flag_e = False
                         break
                 else:
-                    print(f'Error in line {i+1}: Undefined Register name')
-                    flag_d = False
-                    break
-            else:
-                print(f'Error in line {i+1}: Wrong Instruction syntax for {inst_lst[i][0].lower()}')
-                flag_d = False
-                break
-        
-        elif inst_lst2[i][0].lower() in type_e:
-            flag_e = True
-            if len(inst_lst2[i]) == 2:
-                if inst_lst2[i][1].lower() in lab_dic:
-                    op = type_e[inst_lst2[i][0]]
-                    mem = lab_dic[inst_lst2[i][1].lower()]
-                    machine_code.append(op+"000"+mem)
-                elif inst_lst2[i][1].lower() in vars:
-                    print(f'Error in line {i+1}: Use of variables as labels')
-                    flag_d = False
-                    break
-                else:
-                    print(f'Error in line {i+1}: Memory address is not a label')
+                    print(f'Error in line {i+1}: Wrong Instruction syntax for {inst_lst2[i][0].lower()}')
                     flag_e = False
                     break
-            else:
-                print(f'Error in line {i+1}: Wrong Instruction syntax for {inst_lst2[i][0].lower()}')
-                flag_e = False
-                break
         
                 
-        elif inst_lst2[i][0].lower() == "hlt":
-            machine_code.append("0101000000000000")
+            elif inst_lst2[i][0].lower() == "hlt":
+                machine_code.append("0101000000000000")
+                flag_h=True
 
-        elif inst_lst2[i][0].lower() not in opcode and (inst_lst2[i][0].lower() != "var") and (":" not in inst_lst2[i][0].lower()):
-            print(f'Error in line {i+1}: Not an instruction syntax')
-            flag = False
+            elif inst_lst2[i][0].lower() not in opcode and (inst_lst2[i][0].lower() != "var") and (":" not in inst_lst2[i][0].lower()):
+                print(f'Error in line {i+1}: Not an instruction syntax')
+                flag = False
+                break
+        else:
             break
 
-hlt_count = sum([inst.count("hlt") for inst in inst_lst2])
-
-if flag and (len(inst_lst2[-1]) != 1 or inst_lst2[-1][0] != "hlt"):
-    flag = False
-    print(f'Error in line {len(inst_lst2)}: Invalid/Absent hlt declaration')
-    
-for i in range(vars_count):
-    if inst_lst2[0][0].lower() == "var":
-        inst_lst2.remove(inst_lst2[0])
-
-for i in range(len(inst_lst2)):
-    if flag and inst_lst2[i][0] == "var":
+    if flag and (len(inst_lst2[-1]) != 1 or inst_lst2[-1][0] != "hlt"):
         flag = False
-        print(f'Error in line {vars_line[inst_lst2[i][1]]}: Variable not declared at the begining')
+        print(f'Error in line {len(inst_lst2)}: Invalid/Absent hlt declaration')
+    
+    for i in range(vars_count):
+        if inst_lst2[0][0].lower() == "var":
+            inst_lst2.remove(inst_lst2[0])
+
+    for i in range(len(inst_lst2)):
+        if flag and inst_lst2[i][0] == "var":
+            flag = False
+            print(f'Error in line {vars_line[inst_lst2[i][1]]}: Variable not declared at the begining')
 
 if len(machine_code) > 256:
     print(f'Number of instructions exceed limit')
